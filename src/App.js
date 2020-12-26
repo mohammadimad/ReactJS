73518ae55dc863f8d8858c85142a094df7c3e18f
mohammad abdelfattah <hammodaimad@gmail.com>
2020-12-26 10:49:58 +0200

    Aadd file Mohammad

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
 
+ function AgeComponent(props) {
+  let ageTemplate = '';
+  if(props.age > 5) {
+    ageTemplate = <h1>you are older than 5</h1>
+  } else {
+     ageTemplate = <p>you are not older than 5</p>
+   }
+   return ageTemplate;
+ }
+ function UserList(props) {
+   const users = props.users;
+   return ( <ul>
+    {users.map((user, index) => <li key={user.id}>{index} - {user.name}</li>)}
+  </ul>);
+ }
 class App extends Component {
   render() {
     const age = 10;
-    let ageTemplate = '';
-    let user = [
-      {id: 1, name: 'Mohammad', age: age}
-    ]
-    if(age > 5) {
-      ageTemplate = <h1>you are older than 5</h1>
-    } else {
-       ageTemplate = <p>you are not older than 5</p>
-    }
+    let users = [
+      {id: 1, name: 'Mohammad', age: 16},
+      {id: 2, name: 'Wasf', age: 34},
+      {id: 3, name: 'Shad', age: 24},
+    ];
+   
     return (
       <div className="App">
-      {ageTemplate}
+      <AgeComponent age={age} />
+      <AgeComponent age={3} />
+      <UserList users={users}/>
     </div>
-  );
-}
-}
+    );
+  }
+ }
 export default App;
