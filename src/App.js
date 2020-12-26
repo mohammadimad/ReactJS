233bb72b7ffc2a84f59cd0361ec7a294357f695a
mohammad abdelfattah <hammodaimad@gmail.com>
2020-12-26 19:12:34 +0200

    Add file Mohammad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    todos: [
+      {text: 'learn ReactJS'},
+      {text: 'learn NodeJS'},
+      {text: 'learn VueJS'},
+      {text: 'learn AngularJS'},
+    ]
+  }
+  completeTask = (index) => {
+    const todos = [...this.state.todos];
+
+    this.splice(index, 1);
+
+    this.setState({todos});
+  }
+  addTask = () => {
+    const todos = [...this.state.todos];
+    todos.push({
+      text: this.state.newTask
+    });
+  }
   render() {
-   const todos = [
-     {text: 'learn ReactJS'},
-     {text: 'learn NodeJS'},
-   ]
     return (
       <div className="App">
-      {todos.map((todo, index) => <li key={index}>
-        {todo.text} <button>Done</button>
+      {this.state.todos.map((todo, index) => <li key={index}>
+        {todo.text}
+         <button onClick={() =>{this.completeTask(index)}}>
+          Done
+        </button>
         </li>)}
+
+        <input value={this.state.newTask} />
+        <button onClick={addTask}>Add Task</button>
       </div>
     );
   }
